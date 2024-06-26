0dff1f3076b195561b61ecafa8c07afeb9a79e3f
GodMode Json <nusetorfoster77@gmail.com>
2024-06-26 22:57:34 +0000

    intersection observer

--- src/app/focus/focus.component.ts
+++ src/app/focus/focus.component.ts
@@ -1,4 +1,5 @@
-import { Component, AfterViewInit, ElementRef, Renderer2, ViewChildren, QueryList } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, Renderer2, ViewChildren, QueryList, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-focus',
@@ -10,25 +11,27 @@ export class FocusComponent implements AfterViewInit {
   @ViewChildren('focusLeftPanel') focusLeftPanels!: QueryList<ElementRef>;
   @ViewChildren('focusRightPanel') focusRightPanels!: QueryList<ElementRef>;
 
-  constructor(private renderer: Renderer2) {}
+  constructor(private renderer: Renderer2, @Inject(PLATFORM_ID) private platformId: Object) {}
 
   ngAfterViewInit() {
-    const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          this.renderer.addClass(entry.target, 'in-view');
-        } else {
-          this.renderer.removeClass(entry.target, 'in-view');
-        }
+    if (isPlatformBrowser(this.platformId) && typeof IntersectionObserver !== 'undefined') {
+      const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            this.renderer.addClass(entry.target, 'in-view');
+          } else {
+            this.renderer.removeClass(entry.target, 'in-view');
+          }
+        });
       });
-    });
 
-    this.focusLeftPanels.forEach(panel => {
-      observer.observe(panel.nativeElement);
-    });
+      this.focusLeftPanels.forEach(panel => {
+        observer.observe(panel.nativeElement);
+      });
 
-    this.focusRightPanels.forEach(panel => {
-      observer.observe(panel.nativeElement);
-    });
+      this.focusRightPanels.forEach(panel => {
+        observer.observe(panel.nativeElement);
+      });
+    }
   }
 }
